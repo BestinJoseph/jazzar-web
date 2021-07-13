8535802a40409326a831a1876b997e284ac105c0
ojce <bestin2maria@gmail.com>
2021-07-13 11:38:10 +0300

    deploy project data 5

--- client/src/actions/projects.js
+++ client/src/actions/projects.js
@@ -3,6 +3,7 @@ import * as api from '../api/projectApi'
 export const getProjects = () => async (dispatch) => {
     try {
         const { data } = await api.fetchProjects()
+        console.log(data)
         dispatch({ type: 'GET_PROJECTS', payload: data.data})
     } catch (err) {
         console.log(err)
