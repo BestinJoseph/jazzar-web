cc188b6de61117556b594e8ecae80d4ae833fe2e
ojce <bestin2maria@gmail.com>
2021-07-13 12:32:59 +0300

    deploy 6

---
+++ server/controllers/project.js
@@ -0,0 +1,10 @@
+import Project from '../models/project.js'
+
+export const getProjects = async (req, res) => {
+    try {
+        const { data } = await Project.find()
+        res.status(200).json(data)
+    } catch (err) {
+        res.status(400).json({errors: err})
+    }
+}
--- server/routes/project.js
+++ server/routes/project.js
@@ -1,15 +1,15 @@
 import express from 'express'
-import { getProjects, getSingleProjects, postProject, deleteProject, updateProject, deleteProjectImage, deleteProjectCategories } from '../controllers/project.js'
-import uploads from '../middlewares/upload.js'
+import { getProjects } from '../controllers/project.js'
+//import uploads from '../middlewares/upload.js' //, getSingleProjects, postProject, deleteProject, updateProject, deleteProjectImage, deleteProjectCategories
 
 const router = express.Router()
 
 router.get('/', getProjects)
-router.get('/:id', getSingleProjects)
-router.post('/', uploads.none(), postProject)
-router.put('/:id', uploads.array('files', 5), updateProject)
-router.put('/:id/image', uploads.none(), deleteProjectImage)
-router.put('/:id/category', uploads.none(), deleteProjectCategories)
-router.delete('/:id', deleteProject)
+// router.get('/:id', getSingleProjects)
+// router.post('/', uploads.none(), postProject)
+// router.put('/:id', uploads.array('files', 5), updateProject)
+// router.put('/:id/image', uploads.none(), deleteProjectImage)
+// router.put('/:id/category', uploads.none(), deleteProjectCategories)
+// router.delete('/:id', deleteProject)
 
 export default router
